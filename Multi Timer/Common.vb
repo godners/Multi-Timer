f90da2150e4ed5cdfd9c75fa331edbfc773b31c7
Godners Ren <Godners@163.Com>
2020-11-15 20:33:24 +0800

    Revert "添加项目文件。"
    This reverts commit 47d6928d714e40acf292cfe796f49ed6a61f8161.

--- Multi Timer/Common.vb
+++
@@ -1,6 +0,0 @@
-﻿Module Common
-    Public Function NowString(dt As DateTime) As String
-        Return dt.ToString("yyyy-MM-dd HH:mm:ss ", Globalization.CultureInfo.CreateSpecificCulture("en-US")) & dt.DayOfWeek
-    End Function
-
-End Module
